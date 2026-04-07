9e290ebebe0a5198587a61b8a8f4bdd6aa3751a3
GabrielaRistea <gabrielaristea27@gmail.com>
2026-04-08 02:19:40 +0300

    add image processing and data compression

--- image_processing.py
+++ image_processing.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def rotate_image(img, angle):
@@ -10,3 +11,32 @@ def rotate_image(img, angle):
 
 def crop_image(img, start_y, end_y, start_x, end_x):
     return img[start_y:end_y, start_x:end_x]
+
+def apply_median_blur(img, ksize=5):
+    return cv2.medianBlur(img, ksize)
+
+def apply_otsu_threshold(img):
+    if len(img.shape) == 3:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = img
+
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return thresh
+
+def calculate_bone_mass(binary_img):
+    white_pixels = np.sum(binary_img == 255)
+    total_pixels = binary_img.size
+    percentage = (white_pixels / total_pixels) * 100
+    return round(percentage, 2)
+
+
+def apply_canny_edge_detection(img):
+    if len(img.shape) == 3:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = img
+
+    edges = cv2.Canny(gray, 50, 150)
+
+    return edges
